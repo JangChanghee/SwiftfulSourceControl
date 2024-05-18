17d0301982b11cb4804bbf2905fb3b71e160acef
JangChanghee <karampung@gmail.com>
2024-05-18 22:14:28 +0900

    Add HomeView

---
+++ SwiftfulSourceControl/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  SwiftfulSourceControl
+//
+//  Created by Changhee Jang on 5/18/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Screen 2")
+    }
+}
+
+#Preview {
+    HomeView()
+}
