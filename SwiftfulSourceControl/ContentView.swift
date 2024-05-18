b39787148febd92e363f679c45ed6f2364023f86
JangChanghee <karampung@gmail.com>
2024-05-19 00:47:41 +0900

    [Clean] Reorder commit message documentation

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -5,6 +5,26 @@
 //  Created by Changhee Jang on 5/18/24.
 //
 
+/*
+ COMMIT MESSAGES
+ 
+ NEW FEATURES:
+ [Feature] Description of the feature
+ 
+ BUG IN PRODUCTION:
+ [Patch] Description of patch
+ 
+ BUG NOT IN PRODUCTION:
+ [Bug] Description of the bug
+ 
+ MUNDANE TASKS:
+ [Clean] Description of changes
+ 
+ RELEASE:
+ [Release] Description of release
+ 
+ */
+
 import SwiftUI
 
 struct ContentView: View {
